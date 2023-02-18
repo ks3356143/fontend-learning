4a71a319cb7c569008a5be5410c639e9bbac15c8
chen junyi <314298729@qq.com>
2023-02-18 10:49:28 +0800

    ts-尚学堂完结

--- tsLearn/src/modules/GameControl.ts
+++ tsLearn/src/modules/GameControl.ts
@@ -5,6 +5,8 @@ import ScorePanel from "./ScorePanel"
 import Food from "./Food"
 
 class GameControl {
+    // 定义一个按钮
+    btn: HTMLElement
     // 定义三个属性
     // 蛇
     snake: Snake
@@ -18,13 +20,20 @@ class GameControl {
     isLive: Boolean = true
 
     constructor() {
+        this.btn = document.getElementById("button")!
         this.snake = new Snake();
         this.food = new Food();
-        this.scorePanel = new ScorePanel();
+        this.scorePanel = new ScorePanel(10, 10);
         this.init()
     }
     // 游戏初始化方法，调用后游戏就开始
     init() {
+        // 点击按钮游戏重新开始
+        this.btn.addEventListener('click', () => {
+            this.isLive = true
+            this.direction = ''
+            this.run()
+        })
         // 绑定键盘按下的事件,addEventListener很重要
         // bind()是创建一个新函数，把this绑定为这个函数的this，所以keyDownHandler的this全部表示未gameControl的实例对象
         document.addEventListener('keydown', this.keyDownHandler.bind(this))
@@ -38,12 +47,24 @@ class GameControl {
         // console.log(this); //打印出document对象，因为是document调用因为addEventListener
     }
 
+    // 定义一个方法，用来检查蛇是否吃到食物
+    checkEat(X: number, Y: number) {
+        if (X === this.food.X && Y === this.food.Y) {
+            // 检查蛇是否吃到了食物
+            this.food.change()
+            console.log('吃到了食物'); // 当我食物应该，1、食物位置变化 2、分数增加 3、蛇变长一截
+            this.scorePanel.addScore()
+            this.snake.addBody() // 蛇加一截，用谁找谁，面向对象的好处
+        }
+    }
+
     // 创建蛇移动方法
     run() {
         // 根据方向this.direction来使蛇位置改变，上：top减少 、下：top增加 、左：left值减少 、右： left值增加
         // 获取蛇现在坐标
         let X = this.snake.X;
         let Y = this.snake.Y;
+
         // 根据按键方向修改X、Y值
         switch (this.direction) {
             case "ArrowUp" || "Up": //这个是js中case“或”方法
@@ -63,21 +84,27 @@ class GameControl {
                 X += 10
                 break
         }
+
+        // 调用checkEat查看吃到食物没有
+        this.checkEat(X, Y)
+
         // 修改蛇的X和Y值
         try {
             this.snake.X = X;
             this.snake.Y = Y;
-        }catch(e:any){
+        } catch (e: any) {
             // 进入到catch弹出一个提示信息
-            alert(e.message)
+            alert(e.message + 'GAME OVER!')
             // 将isLive设置为false
             this.isLive = false
         }
 
-
         // 开启一个定时调用,嵌套调用，随着级别高
         this.isLive && setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30)
+
     }
+
+
 }
 
 export default GameControl;
