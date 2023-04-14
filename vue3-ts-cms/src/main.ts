60daa1245ee6d490b5a484444b5f408518f59554
chen junyi <314298729@qq.com>
2023-04-14 22:29:15 +0800

    学习axios基础包括config、all()、defaults以及interceptors

--- vue3-ts-cms/src/main.ts
+++ vue3-ts-cms/src/main.ts
@@ -2,9 +2,7 @@ import { createApp } from "vue"
 // import ElementPlus from "element-plus"
 // import "element-plus/dist/index.css"
 import "@/service/axios-demo"
-
 import App from "@/App.vue"
 import router from "@/router/index"
 import store from "@/store/index"
-
 createApp(App).use(router).use(store).mount("#app")
--- vue3-ts-cms/src/service/axios-demo.ts
+++ vue3-ts-cms/src/service/axios-demo.ts
@@ -1,23 +1,5 @@
 import axios from "axios"
 
-axios
-    .get("http://httpbin.org/get", {
-        params: {
-            name: "chen",
-            age: 18
-        }
-    })
-    .then((res) => {
-        console.log(res.data)
-    })
-
-axios
-    .post("http://httpbin.org/post", {
-        params: {
-            name: "coderwhy",
-            age: 20
-        }
-    })
-    .then((res) => {
-        console.log(res.data)
-    })
+axios.defaults.baseURL = "http://httpbin.org"
+axios.defaults.timeout = 10000
+axios.defaults.headers["Content-Type"] = "application/json;charset=utf-8"
